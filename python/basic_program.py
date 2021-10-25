ba3472363bd14d157cc3bc238a64501102108ca1
Aman <neogamers21@gmail.com>
2021-10-25 21:01:01 +0530

    number adding program written

--- python/basic_program.py
+++ python/basic_program.py
@@ -2,3 +2,15 @@ print("hello world")
 # dont import such packages which are not installed, it will result in an error
 
 print(10*"hello world\n")
+
+# here is how to take input from users
+# input() function takes input AS A STRING. 
+# use the following calculator app as a reference
+print("You can add three numbers here.")
+print("Enter first number:")
+a=input()
+print("Enter second number:")
+b=input()
+print("Enter third number:")
+c=input()
+print("Sum of a and b and c is", int(a)+int(b)+int(c))

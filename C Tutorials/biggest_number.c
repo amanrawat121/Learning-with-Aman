d2aff16733c64ec20622b49cdca6071bc3d7e54e
Aman <neogamers21@gmail.com>
2021-10-26 16:13:50 +0530

    biggest number comparing tool (in 3 numbers) added

---
+++ C Tutorials/biggest_number.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("This Program check for the biggest number from any 3 you entered.\nPlease enter first number:\n");
+    scanf("%d", &a);
+    printf("Enter second number:\n");
+    scanf("%d", &b);
+    printf("Enter third number:\n");
+    scanf("%d", &c);
+
+    if (a > b && a > c)
+    {
+        printf("The greatest number is %d", a);
+    }
+    else if (b > a && b > c)
+    {
+        printf("The greatest number is %d", b);
+    }
+    else if (c > a && c > b)
+    {
+        printf("The biggest number is %d", c);
+    }
+    else if (a = b = c)
+    {
+        printf("All three numbers are equal");
+    }
+
+    return 0;
+}

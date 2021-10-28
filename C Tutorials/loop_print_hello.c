e44570297b7db3a6654b6bc49d4b7f77087e4677
Aman <neogamers21@gmail.com>
2021-10-28 14:16:36 +0530

    printing hello 100 times with loop program added

---
+++ C Tutorials/loop_print_hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    printf("This program will print 100 hello worlds with 5 lines of code.");
+    for (i = 1; i < 100; i++)
+    {
+        printf("Hello World\n");
+    }
+
+    return 0;
+}

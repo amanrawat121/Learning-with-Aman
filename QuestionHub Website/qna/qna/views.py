40b0c32677aa81d6d95255f334fa8f1e592071ce
Aman <neogamers21@gmail.com>
2022-02-23 19:59:53 +0530

    deleted

--- QuestionHub Website/qna/qna/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-def homepage(request):
-    return render(request, 'universal/homepage.html')
-
-def discover(request):
-    return render(request, 'universal/discover.html')

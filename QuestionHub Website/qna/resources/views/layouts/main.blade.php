2c9b547cd95d7ef43224fb210f3f097c108fe5f2
Aman <neogamers21@gmail.com>
2022-02-23 21:44:26 +0530

    updated & formatted document

--- QuestionHub Website/qna/resources/views/layouts/main.blade.php
+++ QuestionHub Website/qna/resources/views/layouts/main.blade.php
@@ -2,20 +2,56 @@
 <div class="container">
     <div class="row">
         <div class="col">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, nesciunt laboriosam quos veniam delectus enim esse porro ab quis sed? Nesciunt at ipsam in, accusamus ad quam nulla officiis, deserunt rerum ullam soluta saepe vel, excepturi placeat. Voluptatibus asperiores commodi iure quod fuga deserunt recusandae illo obcaecati repudiandae adipisci, nemo culpa architecto laborum sunt repellat, quam blanditiis, numquam maiores. Vel quod perspiciatis illo vitae doloremque commodi temporibus nulla nemo atque! Iste laboriosam at, rerum adipisci, ipsa possimus vel saepe aperiam reiciendis labore, quaerat aliquid. Perspiciatis dolorem tempore sit ad corrupti ipsam. Cumque dolore magnam repudiandae accusamus deserunt aspernatur beatae facilis!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, nesciunt laboriosam quos veniam delectus
+            enim esse porro ab quis sed? Nesciunt at ipsam in, accusamus ad quam nulla officiis, deserunt rerum ullam
+            soluta saepe vel, excepturi placeat. Voluptatibus asperiores commodi iure quod fuga deserunt recusandae illo
+            obcaecati repudiandae adipisci, nemo culpa architecto laborum sunt repellat, quam blanditiis, numquam
+            maiores. Vel quod perspiciatis illo vitae doloremque commodi temporibus nulla nemo atque! Iste laboriosam
+            at, rerum adipisci, ipsa possimus vel saepe aperiam reiciendis labore, quaerat aliquid. Perspiciatis dolorem
+            tempore sit ad corrupti ipsam. Cumque dolore magnam repudiandae accusamus deserunt aspernatur beatae
+            facilis!
         </div>
         <div class="col-7">
-<p class="border-bottom">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic numquam sed odio. Natus quibusdam repellendus totam ratione ducimus beatae placeat commodi. Repellendus nisi porro dicta ipsam sequi! Illo nam quo porro non exercitationem sapiente, rerum consequuntur. Sapiente optio minima non mollitia rerum, repellendus laboriosam aliquam illo consectetur sit asperiores vel.</p>
-<p class="border-bottom">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic numquam sed odio. Natus quibusdam repellendus totam ratione ducimus beatae placeat commodi. Repellendus nisi porro dicta ipsam sequi! Illo nam quo porro non exercitationem sapiente, rerum consequuntur. Sapiente optio minima non mollitia rerum, repellendus laboriosam aliquam illo consectetur sit asperiores vel.</p>
-<p class="border-bottom">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic numquam sed odio. Natus quibusdam repellendus totam ratione ducimus beatae placeat commodi. Repellendus nisi porro dicta ipsam sequi! Illo nam quo porro non exercitationem sapiente, rerum consequuntur. Sapiente optio minima non mollitia rerum, repellendus laboriosam aliquam illo consectetur sit asperiores vel.</p>
-<p class="border-bottom">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic numquam sed odio. Natus quibusdam repellendus totam ratione ducimus beatae placeat commodi. Repellendus nisi porro dicta ipsam sequi! Illo nam quo porro non exercitationem sapiente, rerum consequuntur. Sapiente optio minima non mollitia rerum, repellendus laboriosam aliquam illo consectetur sit asperiores vel.</p>
-<p class="border-bottom">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic numquam sed odio. Natus quibusdam repellendus totam ratione ducimus beatae placeat commodi. Repellendus nisi porro dicta ipsam sequi! Illo nam quo porro non exercitationem sapiente, rerum consequuntur. Sapiente optio minima non mollitia rerum, repellendus laboriosam aliquam illo consectetur sit asperiores vel.</p>
-<p class="border-bottom">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic numquam sed odio. Natus quibusdam repellendus totam ratione ducimus beatae placeat commodi. Repellendus nisi porro dicta ipsam sequi! Illo nam quo porro non exercitationem sapiente, rerum consequuntur. Sapiente optio minima non mollitia rerum, repellendus laboriosam aliquam illo consectetur sit asperiores vel.</p>
-<p class="border-bottom">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic numquam sed odio. Natus quibusdam repellendus totam ratione ducimus beatae placeat commodi. Repellendus nisi porro dicta ipsam sequi! Illo nam quo porro non exercitationem sapiente, rerum consequuntur. Sapiente optio minima non mollitia rerum, repellendus laboriosam aliquam illo consectetur sit asperiores vel.</p>
-<p class="border-bottom">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic numquam sed odio. Natus quibusdam repellendus totam ratione ducimus beatae placeat commodi. Repellendus nisi porro dicta ipsam sequi! Illo nam quo porro non exercitationem sapiente, rerum consequuntur. Sapiente optio minima non mollitia rerum, repellendus laboriosam aliquam illo consectetur sit asperiores vel.</p>            {{-- @yield('main-content') --}}
+            <p class="border-bottom">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic numquam sed odio.
+                Natus quibusdam repellendus totam ratione ducimus beatae placeat commodi. Repellendus nisi porro dicta
+                ipsam sequi! Illo nam quo porro non exercitationem sapiente, rerum consequuntur. Sapiente optio minima
+                non mollitia rerum, repellendus laboriosam aliquam illo consectetur sit asperiores vel.</p>
+            <p class="border-bottom">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic numquam sed odio.
+                Natus quibusdam repellendus totam ratione ducimus beatae placeat commodi. Repellendus nisi porro dicta
+                ipsam sequi! Illo nam quo porro non exercitationem sapiente, rerum consequuntur. Sapiente optio minima
+                non mollitia rerum, repellendus laboriosam aliquam illo consectetur sit asperiores vel.</p>
+            <p class="border-bottom">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic numquam sed odio.
+                Natus quibusdam repellendus totam ratione ducimus beatae placeat commodi. Repellendus nisi porro dicta
+                ipsam sequi! Illo nam quo porro non exercitationem sapiente, rerum consequuntur. Sapiente optio minima
+                non mollitia rerum, repellendus laboriosam aliquam illo consectetur sit asperiores vel.</p>
+            <p class="border-bottom">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic numquam sed odio.
+                Natus quibusdam repellendus totam ratione ducimus beatae placeat commodi. Repellendus nisi porro dicta
+                ipsam sequi! Illo nam quo porro non exercitationem sapiente, rerum consequuntur. Sapiente optio minima
+                non mollitia rerum, repellendus laboriosam aliquam illo consectetur sit asperiores vel.</p>
+            <p class="border-bottom">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic numquam sed odio.
+                Natus quibusdam repellendus totam ratione ducimus beatae placeat commodi. Repellendus nisi porro dicta
+                ipsam sequi! Illo nam quo porro non exercitationem sapiente, rerum consequuntur. Sapiente optio minima
+                non mollitia rerum, repellendus laboriosam aliquam illo consectetur sit asperiores vel.</p>
+            <p class="border-bottom">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic numquam sed odio.
+                Natus quibusdam repellendus totam ratione ducimus beatae placeat commodi. Repellendus nisi porro dicta
+                ipsam sequi! Illo nam quo porro non exercitationem sapiente, rerum consequuntur. Sapiente optio minima
+                non mollitia rerum, repellendus laboriosam aliquam illo consectetur sit asperiores vel.</p>
+            <p class="border-bottom">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic numquam sed odio.
+                Natus quibusdam repellendus totam ratione ducimus beatae placeat commodi. Repellendus nisi porro dicta
+                ipsam sequi! Illo nam quo porro non exercitationem sapiente, rerum consequuntur. Sapiente optio minima
+                non mollitia rerum, repellendus laboriosam aliquam illo consectetur sit asperiores vel.</p>
+            <p class="border-bottom">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic numquam sed odio.
+                Natus quibusdam repellendus totam ratione ducimus beatae placeat commodi. Repellendus nisi porro dicta
+                ipsam sequi! Illo nam quo porro non exercitationem sapiente, rerum consequuntur. Sapiente optio minima
+                non mollitia rerum, repellendus laboriosam aliquam illo consectetur sit asperiores vel.</p>
+            {{-- @yield('main-content') --}}
         </div>
         <div class="col mt-3">
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa eveniet consectetur minima tempora adipisci iure alias neque excepturi earum maiores numquam id sunt odio, deserunt quisquam iste dicta magni rerum saepe architecto nam amet voluptates! Laudantium fugit corporis harum, natus dolores, optio quaerat reprehenderit iste in voluptatibus minima assumenda. Illum officiis, non fugiat iste illo ullam minus vero omnis? Voluptates?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa eveniet consectetur minima tempora adipisci
+            iure alias neque excepturi earum maiores numquam id sunt odio, deserunt quisquam iste dicta magni rerum
+            saepe architecto nam amet voluptates! Laudantium fugit corporis harum, natus dolores, optio quaerat
+            reprehenderit iste in voluptatibus minima assumenda. Illum officiis, non fugiat iste illo ullam minus vero
+            omnis? Voluptates?
         </div>
     </div>
 </div>

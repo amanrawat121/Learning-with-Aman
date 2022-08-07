a01218ba26d9be9038e40908281aafa558809ad0
amanrawat121 <>
2022-08-07 15:30:03 +0000

    fresh code for this program

--- .ccls-cache/@home@runner@01insertionenddublylinkedlist/main.c
+++ .ccls-cache/@home@runner@01insertionenddublylinkedlist/main.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
-#include <stdio.h>
-struct node
-{
+struct node {
   int data;
   struct node *left;
   struct node *right;
-}*head, *first, *second, *third;
-struct node *createnode(int val)
-{
+} * head, *first, *second, *third;
+struct node *createnode(int val) {
   struct node *newnode = (struct node *)malloc(sizeof(struct node));
   newnode->data = val;
   newnode->left = NULL;
   newnode->right = NULL;
   return newnode;
 }
-void display(struct node *head)
-{
-  while(head!=NULL)
-    {
-      printf("%d\t", head->data);
-      head = head->right;
-    }
+void display(struct node *head) {
+  while (head != NULL) {
+    printf("%d\t", head->data);
+    head = head->right;
+  }
 }
-int main()
-{
+int main() {
   first = createnode(1);
   second = createnode(2);
   third = createnode(3);
@@ -31,5 +25,6 @@ int main()
   second->right = third;
   head = first;
   display(head);
+  insertend(4);
   return 0;
 }
